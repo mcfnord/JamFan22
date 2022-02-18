d3658c1cd17ce83d1e08707138d526600ea99842
jrd <mcfnord@gmail.com>
2022-02-17 21:02:29 -0800

    attempt ssl

--- JamFan22/Program.cs
+++ JamFan22/Program.cs
@@ -1,8 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(80);
+    serverOptions.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("jamfan.pfx", "jamfan"));
+});
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+
+
+/*
 builder.Services.Configure<Microsoft.AspNetCore.Server.Kestrel.KestrelServerOptions>(options =>
 {
     options.ConfigureHttpsDefaults(options =>
@@ -12,6 +22,7 @@ builder.Services.Configure<Microsoft.AspNetCore.Server.Kestrel.KestrelServerOpti
         listenOptions.UseHttps("jamfan.pfx", "jamfan");
     });
 });
+*/
 
 
 var app = builder.Build();
