a2be5ec4c59527aeabfc195e066c2e05400d9cbb
jrd <gh22.x.jrd@xoxy.net>
2022-10-08 18:01:27 +0000

    faster, and get short silence

--- JamFan22/sample.sh
+++ JamFan22/sample.sh
@@ -14,8 +14,15 @@ mv /root/JamFan22/JamFan22/wwwroot/mp3s/output.mp3 /root/JamFan22/JamFan22/wwwro
 # detect silence if it's there.
 ffmpeg -i /root/JamFan22/JamFan22/wwwroot/mp3s/$line.mp3 -af silencedetect=noise=0.0001 -f null - > silence_scan_results 2>&1
 
-# look for silence in the results and kill if found
+# look for silence in the results and kill if found, for the ten
 grep -q 'silence_duration: 10' silence_scan_results
+if [ $? -eq 0 ]
+then
+rm /root/JamFan22/JamFan22/wwwroot/mp3s/$line.mp3
+fi
+
+# look for silence in the results and kill if found, for the nine
+grep -q 'silence_duration: 9' silence_scan_results
 if [ $? -eq 0 ]
 then
 rm /root/JamFan22/JamFan22/wwwroot/mp3s/$line.mp3
@@ -23,6 +30,6 @@ fi
 
 done < "serversToSample.txt"
 
-sleep 360
+sleep 120
 done
 
