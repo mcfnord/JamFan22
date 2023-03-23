abdc85c703a2180be6d94db02db300d40a7f1096
jrd <gh22.x.jrd@xoxy.net>
2023-03-23 18:27:23 +0000

    crawl the empty instead.

--- find_me_and_probe_there.py
+++ find_me_and_probe_there.py
@@ -2,7 +2,6 @@ import os
 import time
 import json
 import urllib.request
-import hashlib
 
 scan_endpoint = [
     'http://143.198.104.205/servers.php?central=anygenre1.jamulus.io:22124',
@@ -16,60 +15,60 @@ scan_endpoint = [
 
 # this script runs on jf, which doesn't have a probe onboard.
 
-probe = ""
-isLoungeFree = str(urllib.request.urlopen("http://lounge.jamulus.live/free.txt").readline().decode('utf-8').strip())
-if isLoungeFree == "True":
-    probe = "lounge"
-else:
-    isRadioFree = str(urllib.request.urlopen("http://radio.jamulus.live/free.txt").readline().decode('utf-8').strip())
-    if isRadioFree == "True":
-        probe = "radio"
+print("hey, only run this during severe lulls. quit now if it's not a lull.")
+time.sleep(5)
 
-if probe == "":
-    print("no probes available.")
-    exit()
+while True:
+    probe = ""
+    isLoungeFree = str(urllib.request.urlopen("http://lounge.jamulus.live/free.txt").readline().decode('utf-8').strip())
+    if isLoungeFree == "True":
+        probe = "lounge"
+    else:
+        isRadioFree = str(urllib.request.urlopen("http://radio.jamulus.live/free.txt").readline().decode('utf-8').strip())
+        if isRadioFree == "True":
+            probe = "radio"
 
-print("using " + probe + ".jamulus.live recklessly.")
+    if probe == "":
+        print("no probes available.")
+        exit()
 
-for url in scan_endpoint:
-    data = json.loads(urllib.request.urlopen(url).read())
-    for server in data:
-        if 'clients' in server:
-            for client in server['clients']:
-                bytes = (client['name'] + client['country'] + client['instrument']).encode('utf-8')
-                theirguid = hashlib.md5(bytes).hexdigest()
+    print("using " + probe + ".jamulus.live recklessly.")
 
-                if 'd7da0b649b4c4f01bfbceed3bb9008fa' == theirguid:
-                    where_john_is = server['ip'] + ":" + str(server['port'])
+    # since i can't run the probe for a long time, i'll just do one probe and quit.
 
-                    denylist = str(urllib.request.urlopen("https://jamulus.live/cannot-dock.txt").read())
-                    if denylist.find(where_john_is) != -1:      # on denylist.
-                        print("This destination is already on the denylist.")
-                        exit()
-                    else:
-                        allowlist = str(urllib.request.urlopen("https://jamulus.live/can-dock.txt").read())
-                        if allowlist.find(where_john_is) != -1: # on allowlist.
-                            print("this destination is already on the allowlist.")
-                            exit()
-                        else:
+    for url in scan_endpoint:
+        data = json.loads(urllib.request.urlopen(url).read())
+        for server in data:
+            if 'clients' in server:
+                continue
 
-                            # is there already a lobby there? that breaks my test
-                            occupants = str(urllib.request.urlopen("http://143.198.104.205/servers.php?server=" + where_john_is).read())
-                            if occupants.find("obby") == -1:        # no lobby there. ok to try one.
+            # we found a server that has no clients on it right now.
+            ipPort = server['ip'] + ":" + str(server['port'])
 
-                                # Request a probe by writing my location on the lounge server.
-                                DEPLOYMENT_REQUEST_FILE = "/root/JamFan22/JamFan22/wwwroot/requested_on_" + probe + ".txt"
-                                os.system("sudo sh -c 'echo " + where_john_is + " > " + DEPLOYMENT_REQUEST_FILE + "'")
-                                time.sleep(20) # wait for the probe to arrive
-                                didlobbyarrive = str(urllib.request.urlopen("http://143.198.104.205/servers.php?server=" + where_john_is).read())
-                                if didlobbyarrive.find("obby") != -1:
-                                    print(where_john_is + " probe succeeded.")
-                                    os.system("sudo sh -c 'echo " + where_john_is + " >> /root/JamFan22/JamFan22/wwwroot/can-dock.txt'")    # add to allowlist
-                                    os.system("sudo sh -c 'grep -v '" + probe + ".jamulus' /root/JamFan22/JamFan22/wwwroot/map.txt > /root/JamFan22/JamFan22/wwwroot/map.txt") # erase from map
-                                    os.system("sudo sh -c 'echo " + where_john_is + " https://" + probe + ".jamulus.live >> /root/JamFan22/JamFan22/wwwroot/map.txt'") # add to map
-                                else:
-                                    os.system("sudo sh -c 'echo " + where_john_is + " >> /root/JamFan22/JamFan22/wwwroot/cannot-dock.txt'") # add to denylist
-                                    print(where_john_is + " probe failed.")
-                                    os.system("sudo sh -c 'echo 127.0.0.1 > " + DEPLOYMENT_REQUEST_FILE + "'") # don't leave this probe hanging
-                            exit()
+            # is it on the denylist?
+            denylist = str(urllib.request.urlopen("https://jamulus.live/cannot-dock.txt").read())
+            if denylist.find(ipPort) != -1:      # on denylist.
+                continue
 
+            # is it on the allowlist?
+            allowlist = str(urllib.request.urlopen("https://jamulus.live/can-dock.txt").read())
+            if allowlist.find(ipPort) != -1: # on allowlist.
+                continue
+
+            # Request a probe by writing my location on the lounge server.
+            print("Probing " + ipPort + " aka " + server['name'])
+            DEPLOYMENT_REQUEST_FILE = "/root/JamFan22/JamFan22/wwwroot/requested_on_" + probe + ".txt"
+            os.system("sudo sh -c 'echo " + ipPort + " > " + DEPLOYMENT_REQUEST_FILE + "'")
+
+            time.sleep(20) # wait for the probe to arrive
+
+            didlobbyarrive = str(urllib.request.urlopen("http://143.198.104.205/servers.php?server=" + ipPort).read())
+            if didlobbyarrive.find("obby") != -1:
+                print("probe succeeded.")
+                os.system("sudo sh -c 'echo " + ipPort + " >> /root/JamFan22/JamFan22/wwwroot/can-dock.txt'")    # add to allowlist
+                # os.system("sudo sh -c 'echo 127.0.0.1 > " + DEPLOYMENT_REQUEST_FILE + "'") # don't leave this probe hanging
+            else:
+                print("probe failed.")
+                os.system("sudo sh -c 'echo " + ipPort + " >> /root/JamFan22/JamFan22/wwwroot/cannot-dock.txt'") # add to denylist
+            break
+    time.sleep(60) # just unclear about speed.
