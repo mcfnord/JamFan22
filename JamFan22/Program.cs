4f52ad3b11dceac3f5a305a308c84ef45a1395e8
jrd <gh22.x.jrd@xoxy.net>
2024-02-09 20:36:54 +0000

    time marches on

--- JamFan22/Program.cs
+++ JamFan22/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseKestrel(serverOptions =>
 {
 //    serverOptions.ListenAnyIP(80);
-    serverOptions.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("keyNov23.pfx", "jamfan"));
+    serverOptions.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("keyFeb24.pfx", "jamfan"));
 });
 //*/
 
