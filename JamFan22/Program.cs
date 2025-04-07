e1d1824c49c88e4a8ae89d81aae10dc5d115eecd
jrd <jf24.x.jrd@xoxy.net>
2025-04-07 23:30:59 +0000

    this matters

--- JamFan22/Program.cs
+++ JamFan22/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseKestrel(serverOptions =>
 {
 //    serverOptions.ListenAnyIP(80);
-    serverOptions.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("keyOct24.pfx", "jamfan"));
+    serverOptions.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("keyJan25.pfx", "jamfan"));
 });
 //*/
 
